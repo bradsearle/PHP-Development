41a665023eae593bce4b415bd06c37892647b4be
Brad Searle <basearle@gmail.com>
2017-05-01 15:08:42 -0500

    adding files
    Signed-off-by: Brad Searle <basearle@gmail.com>

---
+++ dynamic_text.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>TITLE</title>
+    <meta name="description" content="DESCRIPTION">
+   <link rel="stylesheet" href="PATH">
+
+    <!--[if lt IE 9]>
+      <script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+
+
+
+ <p><?php echo $title; ?></p>
+</body>
+
+</html>
